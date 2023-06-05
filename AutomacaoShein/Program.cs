b37d30808555a4a4ccd33a5d0df74c2131736fc5
LucasAndradeSilva <lucasandradesilva2019@gmail.com>
2023-06-05 10:49:24 -0300

    Add project files.

---
+++ AutomacaoShein/Program.cs
@@ -0,0 +1,94 @@
+﻿using Castle.Core.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using System.Diagnostics;
+using Helpers;
+using System.Runtime.InteropServices;
+
+namespace AutomacaoShein
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Pontos Shein";
+                
+            // Appium server
+            var processServer = new Process();
+            processServer.StartInfo.FileName = "cmd.exe"; // Caminho completo para o executável do emulador
+            processServer.StartInfo.Arguments = "/C appium"; // Argumentos para iniciar o emulador com o AVD desejado
+            processServer.StartInfo.CreateNoWindow = false;                        
+            processServer.Start();            
+
+            // Abre emulador
+            var processEmulador = new Process();
+            processEmulador.StartInfo.FileName = "C:\\Android\\android-sdk\\emulator\\emulator.exe"; // Caminho completo para o executável do emulador
+            processEmulador.StartInfo.Arguments = "-avd AutomacaoCelular"; // Argumentos para iniciar o emulador com o AVD desejado
+            processEmulador.StartInfo.CreateNoWindow = false;                        
+            processEmulador.Start();
+
+            //Appium
+            var appiumOptions = new AppiumOptions();
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.NewCommandTimeout, 3600);
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, "AutomacaoCelular");
+            appiumOptions.AddAdditionalCapability("appPackage", "com.zzkko");
+            appiumOptions.AddAdditionalCapability("appActivity", "com.shein.welcome.WelcomeActivity");
+
+            var androidDriver = new AndroidDriver<AppiumWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"), appiumOptions);
+
+            Thread.Sleep(8000);
+
+            // Clique botão "Go Shopping"
+            androidDriver.FindElement(By.Id("com.zzkko:id/n2")).Click();
+
+            Thread.Sleep(2000);
+
+            // Fecha modal cupom
+            try
+            {
+                androidDriver.FindElement(By.Id("com.zzkko:id/ay7")).Click();
+                Thread.Sleep(300);
+            }
+            catch (Exception ex)
+            {
+            }
+
+            // Clique botão "Menu"
+            androidDriver.FindElement(By.Id("com.zzkko:id/bht")).Click();
+            Thread.Sleep(300);
+
+            // Clique botão "Sing in"
+            androidDriver.FindElement(By.Id("com.zzkko:id/bix")).Click();
+            Thread.Sleep(3000);
+
+            // Clique botão "Google"
+            androidDriver.FindElement(By.Id("com.zzkko:id/ai0")).Click();
+            Thread.Sleep(7000);
+
+            // Escolhe conta
+            androidDriver.FindElement(By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[2]")).Click();
+            Thread.Sleep(6000);
+
+            // Clica em pontos            
+            SeleniumHelper.WaitForeverElement(androidDriver, By.Id("com.zzkko:id/aaz")).Click();
+            Thread.Sleep(3000);
+
+            // Clica em "Check in"
+            androidDriver.FindElement(By.Id("com.zzkko:id/bbu")).Click();
+            Thread.Sleep(10000);
+            
+            processServer.Kill();
+            processServer.WaitForExit();
+
+            processEmulador.Kill();
+            processEmulador.WaitForExit();
+
+            GC.Collect();            
+
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ AutomacaoShein/SeleniumHelper.cs
@@ -0,0 +1,288 @@
+﻿using Microsoft.Win32;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Helpers
+{
+    public class SeleniumHelper
+    {
+        public static ChromeDriver StartDriver()
+        {
+            string ChromeDriverPath = Environment.CurrentDirectory;
+            string ChromDriverVersion = GetChromDriverVersion(ChromeDriverPath);
+            string ChromeWebVersion = GetWebChromeVersion();
+
+            if (ChromDriverVersion != ChromeWebVersion)
+            {
+                var urlToDownload = GetURLToDownload(ChromeWebVersion);
+                KillAllChromeDriverProcesses();
+                DownloadNewVersionOfChrome(urlToDownload, ChromeDriverPath);
+                string extract = ExtructZip(ChromeDriverPath);
+            }
+
+            var driver = new ChromeDriver(Path.Combine(Environment.CurrentDirectory, "chromedriver.exe"));
+            return driver;
+        }
+        public static IWebElement WaitElement(ChromeDriver driver, By by)
+        {
+            var tentativas = 0;
+            IWebElement webElement = default;
+
+            while (tentativas <= 20)
+            {
+                try
+                {
+                    webElement = driver.FindElement(by);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    tentativas++;
+                }
+                finally
+                {
+                    Thread.Sleep(500);
+                }
+            }
+
+            return webElement;
+        }
+
+        public static AppiumWebElement WaitElement(AndroidDriver<AppiumWebElement> driver, By by)
+        {
+            var tentativas = 0;
+            AppiumWebElement webElement = default;
+
+            while (tentativas <= 20)
+            {
+                try
+                {
+                    webElement = driver.FindElement(by);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    tentativas++;
+                }
+                finally
+                {
+                    Thread.Sleep(500);
+                }
+            }
+
+            return webElement;
+        }
+
+        public static AppiumWebElement WaitForeverElement(AndroidDriver<AppiumWebElement> driver, By by)
+        {            
+            AppiumWebElement webElement = default;
+
+            while (true)
+            {
+                try
+                {
+                    webElement = driver.FindElement(by);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+                finally
+                {
+                    Thread.Sleep(500);
+                }
+            }
+
+            return webElement;
+        }
+
+
+        public static (bool, IWebElement) TryFindElement(ChromeDriver driver, By by)
+        {
+            try
+            {
+                var element = driver.FindElement(by);
+                return (true, element);
+            }
+            catch (NoSuchElementException ex)
+            {                
+                return (false, null);
+            }            
+        }
+
+        public static bool IsElementVisible(IWebElement element)
+        {
+            if (element is null)            
+                return false;
+            
+            return element.Displayed && element.Enabled;
+        }
+        private static string GetChromDriverVersion(string ChromeDriverePath)
+        {
+            string driverversion = "";
+            if (File.Exists(ChromeDriverePath + "\\chromedriver.exe"))
+            {
+                IWebDriver driver = new ChromeDriver(Path.Combine(Environment.CurrentDirectory, "chromedriver.exe"));
+                ICapabilities capabilities = ((ChromeDriver)driver).Capabilities;
+                driverversion = ((capabilities.GetCapability("chrome") as Dictionary<string, object>)["chromedriverVersion"]).ToString().Split(' ').First();
+                driver.Dispose();
+            }
+            else
+            {
+                Console.WriteLine("ChromeDriver.exe missing !!");
+            }
+
+
+            return driverversion;
+        }
+        private static string GetChromeWebPath()
+        {
+            //Path originates from here: https://chromedriver.chromium.org/downloads/version-selection            
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\chrome.exe"))
+            {
+                if (key != null)
+                {
+                    Object o = key.GetValue("");
+                    if (!String.IsNullOrEmpty(o.ToString()))
+                    {
+                        return o.ToString();
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Unable to get version because chrome registry value was null");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Unable to get version because chrome registry path was null");
+                }
+            }
+        }
+        private static string GetWebChromeVersion()
+        {
+            string productVersionPath = GetChromeWebPath();
+            if (String.IsNullOrEmpty(productVersionPath))
+            {
+                throw new ArgumentException("Unable to get version because path is empty");
+            }
+
+            if (!File.Exists(productVersionPath))
+            {
+                throw new FileNotFoundException("Unable to get version because path specifies a file that does not exists");
+            }
+
+            var versionInfo = FileVersionInfo.GetVersionInfo(productVersionPath);
+            if (versionInfo != null && !String.IsNullOrEmpty(versionInfo.FileVersion))
+            {
+                return versionInfo.FileVersion;
+            }
+            else
+            {
+                throw new ArgumentException("Unable to get version from path because the version is either null or empty: " + productVersionPath);
+            }
+        }
+        private static string GetURLToDownload(string version)
+        {
+            if (String.IsNullOrEmpty(version))
+            {
+                throw new ArgumentException("Unable to get url because version is empty");
+            }
+
+            //URL's originates from here: https://chromedriver.chromium.org/downloads/version-selection
+            string html = string.Empty;
+            string urlToPathLocation = @"https://chromedriver.storage.googleapis.com/LATEST_RELEASE_" + String.Join(".", version.Split('.').Take(3));
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlToPathLocation);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            if (String.IsNullOrEmpty(html))
+            {
+                throw new WebException("Unable to get version path from website");
+            }
+
+            return "https://chromedriver.storage.googleapis.com/" + html + "/chromedriver_win32.zip";
+        }
+        private static void KillAllChromeDriverProcesses()
+        {
+            var processes = Process.GetProcessesByName("chromedriver");
+            foreach (var process in processes)
+            {
+                try
+                {
+                    process.Kill();
+                }
+                catch
+                {
+
+                }
+            }
+        }
+        private static void DownloadNewVersionOfChrome(string urlToDownload, string ChromDriverPath)
+        {
+            if (String.IsNullOrEmpty(urlToDownload))
+            {
+                throw new ArgumentException("Unable to get url because urlToDownload is empty");
+            }
+            using (var client = new WebClient())
+            {
+                if (File.Exists(ChromDriverPath + "\\chromedriver.zip"))
+                {
+                    File.Delete(ChromDriverPath + "\\chromedriver.zip");
+                }
+
+                client.DownloadFile(urlToDownload, "chromedriver.zip");
+
+                if (File.Exists(ChromDriverPath + "\\chromedriver.zip") && File.Exists(ChromDriverPath + "\\chromedriver.exe"))
+                {
+                    File.Delete(ChromDriverPath + "\\chromedriver.exe");
+                }
+
+            }
+        }
+        private static string ExtructZip(string ChromeDriverPath)
+        {
+            string errorMsg = "";
+            string zipFileName = "";
+            try
+            {
+                zipFileName = Path.Combine(Environment.CurrentDirectory, "chromedriver.zip");
+                string orgFileName = "";                
+                using (var zip = ZipFile.OpenRead(zipFileName))
+                {
+                    foreach (var e in zip.Entries)
+                    {
+                        e.ExtractToFile(Path.Combine(Environment.CurrentDirectory, "chromedriver.exe"), true);
+                        orgFileName = e.Name;
+                    }
+                }
+                File.Delete(zipFileName);
+                errorMsg = "Done";
+            }
+            catch (Exception ex)
+            {
+                errorMsg = "ExtructZip  file " + zipFileName.Split('\\').Last() + " - " + ex.Message;
+            }
+            return errorMsg;
+        }
+    }
+}
